a56400162dc5f2d54cb7081b907becceec4eb7ea
Leonardo Severo Pedroso <leopedroso45@gmail.com>
2019-09-01 08:54:42 -0300

    Adding some changes

---
+++ 2.1# Go/findian.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	//var String
+	var stringValue string
+	var originalValue string
+	fmt.Printf("please, type some text:")
+	//user input
+	fmt.Scan(&stringValue)
+	originalValue = stringValue
+	//remove spaces
+	stringValue = removeSpace(stringValue)
+	//stringValue = strings.Replace(stringValue, " ", "", -1)
+	//set string to lower case
+	stringValue = strings.ToLower(stringValue)
+	//if contains substring "i", "a" and "n"
+	if strings.Contains(stringValue, "i") && strings.Contains(stringValue, "a") && strings.Contains(stringValue, "n") {
+		// if "i" is a prefix and "n" a suffix
+		firstChar := strings.HasPrefix(stringValue, "i")
+		lastChar := strings.HasSuffix(stringValue, "n")
+		switch {
+		//if is a prefix and suffix so Found
+		case firstChar && lastChar:
+			fmt.Printf("Found in %s", originalValue)
+		//if isnt a prefix and suffix so Not Found
+		default:
+			fmt.Printf("Not Found in %s", originalValue)
+		}
+		//if there's no "i", "a" and "n"
+	} else {
+		fmt.Printf("Not Found in %s", originalValue)
+	}
+}
+
+func removeSpace(s string) string {
+	rr := make([]rune, 0, len(s))
+	for _, r := range s {
+		if !unicode.IsSpace(r) {
+			rr = append(rr, r)
+		}
+	}
+	return string(rr)
+}
