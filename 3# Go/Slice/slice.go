d37169fe283766f823cc02233bbd31c73215d741
Leonardo Severo Pedroso <leopedroso45@gmail.com>
2019-09-18 14:40:10 -0300

    Slice

---
+++ 3# Go/Slice/slice.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	arr := [...]string{"a", "b", "c", "d", "e", "f", "g"}
+	s1 := arr[1:3]
+	s2 := arr[2:5]
+	fmt.Println(arr)
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+}
