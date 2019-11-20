81e9626e7378b166c0f3c413618c71ec023f5532
Leonardo Severo Pedroso <leopedroso45@gmail.com>
2019-11-20 20:43:01 -0300

    Atualizando master

---
+++ 4# Go/Read/read.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Printf("Hello my friend, could you inform your name?")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Printf("I need to know your address too...")
+	var address string
+	fmt.Scanln(&address)
+
+	peopleMap := map[string]string{
+		"name":    name,
+		"address": address,
+	}
+	barr, err := json.Marshal(peopleMap)
+	f, err := os.Create("test.json")
+	if err == nil {
+		_, err := f.Write(barr)
+		if err == nil {
+			fmt.Println("Done!")
+			for key, val := range peopleMap {
+				fmt.Println("'" + key + "'" + ":" + "'" + val + "'")
+			}
+		}
+	}
+
+	dat, err := ioutil.ReadFile("test.txt")
+	if err == nil {
+		fmt.Println(dat)
+	}
+	err = ioutil.WriteFile("outfile.txt", dat, 0777)
+
+}
